15e6e60fc46abc734b815426f6d33f97cc7dcf93
alamuri123 <113355160+alamuri123@users.noreply.github.com>
2022-09-13 14:42:52 +0530

    Create factorial number using recursion.c

---
+++ factorial number using recursion.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int facto(int n)
+{
+    if(n==1)
+        return 1;
+    else
+        return(n*facto(n-1));
+}
+int main()
+{
+    int n,fact;
+    printf("enter the number");
+    scanf("%d",&n);
+    fact=facto(n);
+    printf("factorial of number is%d",fact);
+}
